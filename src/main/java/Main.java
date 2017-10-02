87c97b9d3657078166c2fc2bdb4773b6f7e352e5
nunomota <lostintime@nunomota.io>
2017-10-02 15:38:16 +0200

    Adds main JavaDoc comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,15 @@
+/**
+ * This is a really simple implementation of a Chip8 emulator.
+ *
+ * I chose to make it an 'interpreting' and not a 'recompiling' emulator for the sake of simplicity (as a static recompiling
+ * one has its downfall for self-modifying code, a dynamic one should be chosen instead - which would mean a lot of work)
+ * but I might change this in the future.
+ *
+ * Resources:
+ *    http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+ *    https://en.wikipedia.org/wiki/CHIP-8#Opcode_table
+ *    http://fms.komkon.org/EMUL8/HOWTO.html
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
